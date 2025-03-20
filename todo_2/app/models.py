d5bb4cb0f56dfc3463de1f951b4204287f714826
rogereichert <rogereichert@gmail.com>
2025-03-20 05:18:06 -0300

    Created migrations from file Tarefas

--- todo_2/app/models.py
+++ todo_2/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Tarefa(models.Model):
+    
+    STATUS_CHOICES = (
+        (1, 'A fazer'),
+        (2, 'Fazendo'),
+        (3, 'Concluído')
+    )
+    
+    titulo = models.CharField(max_length=100, blank=False, null=False)
+    descricao = models.TextField(blank=False, null=False)
+    data = models.DateField(null=False, blank=False)
+    status = models.IntegerField(choices=STATUS_CHOICES, null=False, blank=False)
+    

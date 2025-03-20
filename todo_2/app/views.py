62488e18c79a22ea09c785e868b28ff15b1ce1f5
rogereichert <rogereichert@gmail.com>
2025-03-20 10:13:07 -0300

    Include form in form_cadastro.html

--- todo_2/app/views.py
+++ todo_2/app/views.py
@@ -3,5 +3,10 @@ from .forms import TarefaForm
 
 # Create your views here.
 def cadastrar_tarefa(request):
-    form_tarefa = TarefaForm()
+    if request.method == 'POST':
+        form_tarefa = TarefaForm(request.POST)
+        if form_tarefa.is_valid():
+            form_tarefa.save()
+    else:
+        form_tarefa = TarefaForm()
     return render(request, 'form_cadastro.html', {'form_tarefa': form_tarefa})
